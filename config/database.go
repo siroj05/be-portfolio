4aef3f5267dc9387cbe555e740db3eda21162afc
sirojjuddin.apendi <sirojjuddin.apendi@amsconsult.com>
2025-08-12 14:42:10 +0700

    init structure folder

---
+++ config/database.go
@@ -0,0 +1,13 @@
+package config
+
+import "fmt"
+
+/*
+* koneksi ke db disini
+ */
+
+//  var DB *sql.DB
+
+func GetConnection() {
+	fmt.Println("Get connection")
+}
---
+++ routes/messages_routes.go
@@ -0,0 +1,7 @@
+package routes
+
+import "fmt"
+
+func MessagesRoutes() {
+	fmt.Println("Routes")
+}
